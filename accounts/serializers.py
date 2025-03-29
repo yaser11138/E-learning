34d873d99e476d661d4158709cdb53f24ab0a3e7
Yaser Dehghan <ydahgan@gmail.com>
2025-03-29 21:55:26 +0330

    Create Serializer for Instructor

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import  serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
-from .models import Student
+from .models import Student,Instructor
 
 User = get_user_model()
 
@@ -12,6 +12,12 @@ class StudentSerializer(serializers.ModelSerializer):
         model = Student
         fields = ['birth_date', 'education', 'phone_number']
         
+
+class InstructorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Instructor
+        fields = ["bio", "education"]
+
 
 class StudentRegisterSerializer(RegisterSerializer):
     first_name = serializers.CharField(max_length=10)
@@ -38,6 +44,28 @@ class StudentProfileSerializer(serializers.ModelSerializer):
             student = instance.student
         else:
             return serializers.ValidationError("the instance doesn't have student attribute")
+        for attr in validated_data:
+            setattr(instance,attr,validated_data[attr])
+
+        for attr in student_data:
+            setattr(student, attr, student_data[attr])
+
+        return instance
+
+
+class InstructorProfileSerializer(serializers.ModelSerializer):
+    instructor = InstructorSerializer()
+
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "instructor"]
+
+    def update(self, instance, validated_data):
+        student_data = validated_data.pop("instructor")
+        if hasattr(instance, "instructor"):
+            student = instance.student
+        else:
+            return serializers.ValidationError("the instance doesn't have instructor attribute")
         for attr in validated_data:
             setattr(instance,attr,validated_data[attr])
 
