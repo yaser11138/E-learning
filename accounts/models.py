92fe15853c128b759e760d70e027501708ab48b5
Yaser Dehghan <ydahgan@gmail.com>
2025-03-30 12:21:36 +0330

    Rewrite the choices of models

--- accounts/models.py
+++ accounts/models.py
@@ -6,19 +6,16 @@ class CustomUser(AbstractUser):
     pass
 
 
-EDUCATION_CHOICES = [
-    ("HIGH_SCHOOL", "High School"),
-    ("BACHELORS", "Bachelors"),
-    ("MASTERS", "Masters"),
-    ("PHD", "PhD"),
-]
-
-
 class Student(models.Model):
+    class EducationChoices(models.TextChoices):
+        High_school = "HIGH_SCHOOL", "High School"
+        Bachelors = "BACHELORS", "Bachelors"
+        Master = "MASTERS", "Masters"
+        Phd = "PHD", "PhD"
 
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     birth_date = models.DateField()
-    education = models.CharField(max_length=255, choices=EDUCATION_CHOICES)
+    education = models.CharField(max_length=255, choices=EducationChoices.choices)
     phone_number = models.CharField(max_length=255)
     profile_picture = models.ImageField(
         upload_to="profile_pictures/", null=True, blank=True
@@ -26,9 +23,15 @@ class Student(models.Model):
 
 
 class Instructor(models.Model):
+    class EducationChoices(models.TextChoices):
+        High_school = "HIGH_SCHOOL", "High School"
+        Bachelors = "BACHELORS", "Bachelors"
+        Master = "MASTERS", "Masters"
+        Phd = "PHD", "PhD"
+
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     bio = models.TextField()
-    education = models.CharField(max_length=255, choices=EDUCATION_CHOICES)
+    education = models.CharField(max_length=255, choices=EducationChoices.choices)
     profile_picture = models.ImageField(
         upload_to="profile_pictures/", null=True, blank=True
     )
