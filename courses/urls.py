20d41fd72fa669bc249823f8ed00760ef63802a8
Yaser Dehghan <ydahgan@gmail.com>
2025-04-12 10:43:17 +0330

    Create views for Course app

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,11 @@
+from django.urls import path
 from rest_framework import routers
-from .views import CourseViewSet
+from .views import CourseViewSet, ModuleViewSet, ModuleCreateView, ModuleListView
 router = routers.DefaultRouter()
-router.register(r'', CourseViewSet, basename='course')
-
+router.register(r'course', CourseViewSet, basename='course')
+router.register('modules', ModuleViewSet, basename="module")
 urlpatterns = router.urls
+urlpatterns += [
+    path("course/<slug:slug>/create_module/", ModuleCreateView.as_view(), name="module_create"),
+    path("course/<slug:slug>/moudels", ModuleListView.as_view(), name="module_list"),
+]
--- courses/views.py
+++ courses/views.py
@@ -1,13 +1,15 @@
-from rest_framework.viewsets import ModelViewSet
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import status
 from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
 from rest_framework.permissions import IsAuthenticated
 from drf_spectacular.types import OpenApiTypes
-from .serializers import CourseSerializer
+from .serializers import CourseSerializer, ModuleSerializer
 from .permissions import IsInstructor, IsOwner
-from .models import Course
+from .models import Course, Module
 
 
 class CourseViewSet(ModelViewSet):
@@ -54,9 +56,107 @@ class CourseViewSet(ModelViewSet):
         return Response({"message": f"{title} successfully deleted"}, status=status.HTTP_200_OK)
 
 
+class ModuleListView(APIView):
+    serializer_class = ModuleSerializer
+    permission_classes = [IsAuthenticated, IsInstructor, IsOwner]
+
+    @extend_schema(
+        summary="List all modules in a course",
+        description="Retrieve a list of modules associated with a given course slug.",
+        parameters=[
+            OpenApiParameter(name="slug", location=OpenApiParameter.PATH,
+                             required=True, description="The unique identifier of the course.", type=str)
+        ],
+        responses={200: ModuleSerializer(many=True), 404: {"description": "Course not found"}},
+        tags=["module"]
+    )
+    def get(self, request, slug: str = None):
+        course = get_object_or_404(Course, slug=slug)
+        self.check_object_permissions(request, course.owner)
+        course_modules_serializer = ModuleSerializer(instance=course.modules, many=True)
+        return Response(data=course_modules_serializer.data, status=status.HTTP_200_OK)
 
 
+class ModuleCreateView(APIView):
+    serializer_class = ModuleSerializer
+    permission_classes = [IsAuthenticated, IsInstructor]
+    @extend_schema(
+        summary="Create a module",
+        description="Create a new module within a specific course.",
+        parameters=[
+            OpenApiParameter(name="slug", location=OpenApiParameter.PATH,
+                             required=True, description="The unique identifier of the course.", type=str)
+        ],
+        request=ModuleSerializer,
+        responses={201: ModuleSerializer, 400: {"description": "Invalid input data"},
+                   404: {"description": "Course not found"}},
+        tags=["module"]
+    )
+    def post(self, request, slug: str):
+        course = get_object_or_404(Course, slug=slug)
+        serializer = ModuleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(course=course)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class ModuleViewSet(ViewSet):
+    serializer_class = ModuleSerializer
+    permission_classes = [IsAuthenticated, IsInstructor, IsOwner]
+    lookup_field = "slug"
 
+    @extend_schema(
+        summary="Retrieve a module",
+        description="Fetch details of a specific module using its slug.",
+        parameters=[
+            OpenApiParameter(name="slug", location=OpenApiParameter.PATH,
+                             required=True, description="The unique identifier of the module.", type=str)
+        ],
+        responses={200: ModuleSerializer, 404: {"description": "Module not found"}},
+        tags=["module"]
+    )
+    def retrieve(self, request, slug: str = None):
+        module = get_object_or_404(Module, slug=slug)
+        module_serializer = ModuleSerializer(instance=module)
+        return Response(data=module_serializer.data, status=status.HTTP_200_OK)
 
+    @extend_schema(
+        summary="Update a module",
+        description="Update an existing module using its slug.",
+        parameters=[
+            OpenApiParameter(name="slug", location=OpenApiParameter.PATH,
+                             required=True, description= "The unique identifier of the module.", type=str)
+        ],
+        request=ModuleSerializer,
+        responses={200: ModuleSerializer, 400: {"description": "Invalid input data"},
+                   403: {"description": "Permission denied"}, 404: {"description": "Module not found"}},
+        tags=["module"]
+    )
+    def update(self, request, slug: str = None):
+        module = get_object_or_404(Module, slug=slug)
+        self.check_object_permissions(request, module.course.owner)
+        module_serializer = ModuleSerializer(instance=module, data=request.data, partial=True)
+        if module_serializer.is_valid():
+            module_serializer.save()
+            return Response(data=module_serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(data=module_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(
+        summary="Delete a module",
+        description="Delete a specific module using its slug.",
+        parameters=[
+            OpenApiParameter(name="slug", location=OpenApiParameter.PATH, required=True,
+                             description="The unique identifier of the module.", type=str)
+        ],
+        responses={204: {"description": "Module deleted successfully"}, 403: {"description": "Permission denied"},
+                   404: {"description": "Module not found"}},
+        tags=["module"]
+    )
+    def destroy(self, request, slug: str = None):
+        module = get_object_or_404(Module, slug=slug)
+        self.check_object_permissions(request, module.course.owner)
+        module.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
