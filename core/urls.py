464950174b0a563c284ed3e5bf57d402fe389e85
Yaser Dehghan <ydahgan@gmail.com>
2025-04-12 10:44:24 +0330

    change the course url to content_management

--- core/urls.py
+++ core/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('course/', include("courses.urls"))
+    path('content_mangement/', include("courses.urls"))
 ]
 
 if settings.DEBUG:
