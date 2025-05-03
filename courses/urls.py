02fefa1dc368650a17730c59014cf858b16e67bd
Yaser Dehghan <ydahgan@gmail.com>
2025-05-03 11:30:15 +0330

    add Content urls

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework import routers
-from .views import CourseViewSet, ModuleViewSet, ModuleCreateView, ModuleListView
+from .views import CourseViewSet, ModuleViewSet, ModuleCreateView, ModuleListView, ContentViewListCreate, ContentDetailView
 router = routers.DefaultRouter()
 router.register(r'course', CourseViewSet, basename='course')
-router.register('modules', ModuleViewSet, basename="module")
+router.register(r'modules', ModuleViewSet, basename="module")
+router.register(r'contents', ContentDetailView, basename="content")
 urlpatterns = router.urls
 urlpatterns += [
     path("course/<slug:slug>/create_module/", ModuleCreateView.as_view(), name="module_create"),
     path("course/<slug:slug>/moudels", ModuleListView.as_view(), name="module_list"),
+    path("module/<slug:module_slug>/content/", ContentViewListCreate.as_view(), name="module-contents")
 ]
