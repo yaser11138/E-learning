993d4e9a63a96753936acfdb82e94793af447415
Yaser Dehghan <ydahgan@gmail.com>
2025-05-03 11:57:29 +0330

    Create enrollment app and it's model

--- core/settings.py
+++ core/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.sites",
     "accounts",
     "courses",
+    "enrollment",
     "rest_framework",
     "rest_framework.authtoken",
     "allauth",
---
+++ enrollment/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from courses.models import Course
+
+
+User = get_user_model()
+
+
+class Enrollment(models.Model):
+    class StatusChoices(models.TextChoices):
+        Completed = "COMPLETED", "Completed"
+        In_progress = "IN PROGRESS", "In progress"
+        reached_deadline = "REACHED DEADLINE", "reached deadline"
+
+    user = models.ForeignKey(User, related_name="enrollments", on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, related_name="enrollments", on_delete=models.CASCADE)
+    started = models.DateField(auto_now_add=True)
+    deadline = models.DateField()
+    status = models.CharField(choices=StatusChoices.choices)
+
+    @property
+    def deadline_reached(self):
+        if self.deadline == timezone.now().date():
+            return True
+        else:
+            return False
