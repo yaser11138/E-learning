d7fbe0c17ee4255ee33cc9f73028cca012464d36
Yaser Dehghan <ydahgan@gmail.com>
2025-05-14 18:58:05 +0330

    add progress urls

--- courses/student_urls.py
+++ courses/student_urls.py
@@ -1,10 +1,27 @@
 from django.urls import path
 from rest_framework import routers
-from .views import StudentCourseView,StudentContentView
+from .views import (
+    StudentCourseView,
+    StudentContentView,
+    CourseProgressView,
+    ContentProgressView,
+)
+
 router = routers.DefaultRouter()
 router.register("course", StudentCourseView, basename="course_list")
+
 urlpatterns = router.urls
 
 urlpatterns += [
-    path("content/<slug:slug>", StudentContentView.as_view(), name="content_retrieve")
+    path("content/<slug:slug>", StudentContentView.as_view(), name="content_retrieve"),
+    path(
+        "course/<slug:slug>/progress",
+        CourseProgressView.as_view(),
+        name="course_progress",
+    ),
+    path(
+        "content/<slug:slug>/progress",
+        ContentProgressView.as_view(),
+        name="content_progress",
+    ),
 ]
